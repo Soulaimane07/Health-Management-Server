151f3d25fd906e89b8b8ada440b7d99ce656afe1
Soulaimane <105684836+Soulaimane07@users.noreply.github.com>
2023-03-13 22:08:42 +0100

    all

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express")
 const app = express()
+const morgan = require('morgan')
 
 const usersRoutes = require('./api/routes/users')
 const usersDetailsRoutes = require("./api/routes/userDetails")
 
+app.use(morgan('dev'))
+
 app.use('/users', usersRoutes)
 app.use('/usersDetails', usersDetailsRoutes)
+
+app.use((req, res, next) => {
+    const error = new Error('Not fount')
+    error.status(404)
+    next(error)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
 
 module.exports = app
