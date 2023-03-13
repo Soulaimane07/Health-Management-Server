2638343afbd41faf11f4b925cbbbe91773970ee8
Soulaimane <105684836+Soulaimane07@users.noreply.github.com>
2023-03-13 22:59:18 +0100

    mongoose

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ const express = require("express")
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 
 const usersRoutes = require('./api/routes/users')
 const usersDetailsRoutes = require("./api/routes/userDetails")
 
+mongoose.connect('mongodb+srv://soulaimane:QZv1gkNEYwDfGZ2N@pfe.r42jd4t.mongodb.net/?retryWrites=true&w=majority')
+
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    )
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+        return res.status(200).json({})
+    }
+    next( )
+})
 
 app.use('/users', usersRoutes)
 app.use('/usersDetails', usersDetailsRoutes)
