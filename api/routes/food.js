83e96ab02f844b1c1e9c183385aed6dd7c86ab5c
Soulaimane <105684836+Soulaimane07@users.noreply.github.com>
2023-04-12 14:34:06 +0000

    imageUpload

--- api/routes/food.js
+++ api/routes/food.js
@@ -9,7 +9,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, 'uploads/images/food')
+        cb(null, 'uploads')
     },
     filename: function(req, file, cb){
         cb(null, Date.now() + path.extname(file.originalname))
